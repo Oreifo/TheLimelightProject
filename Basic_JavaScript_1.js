1bc960ac4a289ecad2b05bb360c81a07de27fb9b
Oreifo <132648868+Oreifo@users.noreply.github.com>
2023-09-27 17:17:45 +0100

    My first JavaScript code

---
+++ Basic_JavaScript_1.js
@@ -0,0 +1,10 @@
+
+
+alert("Hello,Oreifo! Welcome to the world of JavaScript. This is an alert!!");
+
+function My_First_Function(){
+     var String = "kiss me, mon amor...i'm Italian !";
+	 var result = string.fontcolor("blue");
+	 document.getElementById("Italian").innerHTML =
+	 result;
+} 	 
